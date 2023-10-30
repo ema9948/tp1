2ed5c1a58650e3eab7b4c0b2d76be31f43bedb76
cristian <ema9948@gmail.com>
2023-10-29 22:27:11 -0300

    add ticket

---
+++ index.js
@@ -0,0 +1,72 @@
+(() => {
+  const form = document.getElementById("form");
+  const amount = document.getElementById("amount");
+  const precie = document.getElementById("precie");
+  const clear = document.getElementById("clear");
+
+  form.addEventListener("submit", submit);
+
+  clear.addEventListener("click", clean);
+
+  function clean(value) {
+    amount.classList.add("visually-hidden");
+    form.reset();
+  }
+
+  function submit(value) {
+    value.preventDefault();
+    const formData = new FormData(form);
+
+    if (checkInput(formData)) {
+      return;
+    }
+
+    const data = serializeForm(formData);
+
+    // const json = JSON.stringify(Object.fromEntries(formData));
+
+    precie.textContent = descuento(data);
+    amount.classList.remove("visually-hidden");
+  }
+
+  const descuento = (e) => {
+    const offSale = [80, 50];
+    if (e.categoria < 0) {
+      Swal.fire({
+        title: "Error!",
+        text: `Categoría Errónea!`,
+        icon: "error",
+        confirmButtonText: "Cool",
+      });
+      return;
+    }
+
+    return e.cantidad * (200 - 200 * (offSale[e.categoria] / 100));
+  };
+
+  const serializeForm = function (form) {
+    let obj = {};
+    for (var key of form.keys()) {
+      obj[key] = form.get(key);
+    }
+    return obj;
+  };
+
+  const checkInput = (value) => {
+    let status = false;
+    for (var key of value.keys()) {
+      if (!value.get(key)) {
+        console.log(key);
+        Swal.fire({
+          title: "Error!",
+          text: `Complete el campo " ${key.toUpperCase()} "`,
+          icon: "error",
+          confirmButtonText: "Cool",
+        });
+        status = true;
+        break;
+      }
+    }
+    return status;
+  };
+})();
